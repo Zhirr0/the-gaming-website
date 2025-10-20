1727d0fdaf38f420c982d00256935a85dfc7ac4a
Zhir Bakhtyar <zheerbaxtyar9@gmail.com>
2025-10-20 22:24:05 +0300

    Finally made the css more modern and rewrote the css file in the new version of tailwindcss

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,10 @@ const Hero = () => {
         id="video-frame"
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
       >
+
+        <h1 className="text-green-500">hello world</h1>
         <div>
-          <div className="mask-clip-path">
+          <div className="mask-clip-path absolute-center absolute z-50">
             
           </div>
         </div>
