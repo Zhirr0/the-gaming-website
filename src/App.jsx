e8dfcd35abf5e45811fd79097edb08a4b5955ca8
Zhir Bakhtyar <zheerbaxtyar9@gmail.com>
2025-10-21 19:53:03 +0300

    Creating the Hero section after alot of effort and adding lenis to make the scrolling smooth

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { useRef, useEffect } from "react"
+import gsap from "gsap"
+import { ReactLenis } from "lenis/react"
 import Hero from "./components/Hero"
 
 const App = () => {
+  const lenisRef = useRef()
+
+  useEffect(() => {
+    function update(time) {
+      lenisRef.current?.lenis?.raf(time * 1000)
+    }
+
+    gsap.ticker.add(update)
+    return () => gsap.ticker.remove(update)
+  }, [])
+
   return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <Hero />
-      
-    </main>
+    <ReactLenis root options={{ autoRaf: false }} ref={lenisRef}>
+      <main className="relative min-h-screen w-screen overflow-x-hidden">
+        <Hero />
+
+        <section className="min-h-screen bg-blue-500"></section>
+      </main>
+    </ReactLenis>
   )
 }
 
