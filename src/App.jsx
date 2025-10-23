202537d44ddabe36701685b66d4a1f6499a2d743
Zhir Bakhtyar <zheerbaxtyar9@gmail.com>
2025-10-23 20:43:26 +0300

    Created the Contact section and the Footer section and made some changes to the ClipBox it had some problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,39 @@
-import { useRef, useEffect } from "react"
-import gsap from "gsap"
-import { ReactLenis } from "lenis/react"
-import Hero from "./components/Hero"
-import About from "./components/About"
-import Navbar from "./components/Navbar"
-import Features from "./components/Features"
-import Story from "./components/Story"
+import { useRef, useEffect } from "react";
+import gsap from "gsap";
+import { ReactLenis } from "lenis/react";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Features from "./components/Features";
+import Story from "./components/Story";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
-  const lenisRef = useRef()
+  const lenisRef = useRef();
 
   useEffect(() => {
     function update(time) {
-      lenisRef.current?.lenis?.raf(time * 1000)
+      lenisRef.current?.lenis?.raf(time * 1000);
     }
 
-    gsap.ticker.add(update)
-    return () => gsap.ticker.remove(update)
-  }, [])
+    gsap.ticker.add(update);
+    return () => gsap.ticker.remove(update);
+  }, []);
 
   return (
     <ReactLenis root options={{ autoRaf: false }} ref={lenisRef}>
-
       <main className="relative min-h-screen w-screen overflow-x-hidden">
         <Navbar />
         <Hero />
         <About />
         <Features />
         <Story />
+        <Contact />
+        <Footer />
       </main>
     </ReactLenis>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/ImgClipBox.jsx
@@ -0,0 +1,8 @@
+const ImgClipBox = ({ src, clipClass }) => (
+  <div className={clipClass}>
+    <img src={src} />
+  </div>
+);
+
+
+export default ImgClipBox;
